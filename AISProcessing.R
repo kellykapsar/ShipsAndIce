add2c2e251516bc5c24c384cf769d700085c20b7
kellykapsar <kelly.kapsar@gmail.com>
2022-02-01 14:47:09 -0600

    Create function to process ice data.

--- AISProcessing.R
+++ AISProcessing.R
@@ -65,74 +65,122 @@ shipinice <- raster::stack(shipinice)
 
 ########################################################################################################
 # Load in all sea ice thickness files 
-thicklist <- as.list(list.files(paste0(wd, "Data_Raw/SeaIceThickness_SMOS_2015-2020/"), pattern='.nc'))
+smoslist <- as.list(list.files(paste0(wd, "Data_Raw/SeaIceThickness_SMOS_2015-2020/"), pattern='.nc'))
 
-thickdates <- lapply(thicklist, function(x){as.Date(paste0(substr(x, start=33, stop=40), "01"), format="%Y%m%d")})
+smosmonths <- lapply(smoslist, function(x){as.numeric(substr(x, start=33, stop=38))})
 
-
-# Open netcdf file 
-
-ice <- nc_open(paste0(wd,"Data_Raw/SeaIceThickness_SMOS_2015-2020/", thicklist[[2]]))
+# Get long and lat matrices (Same for all layers)
+ice <- nc_open(paste0(wd,"Data_Raw/SeaIceThickness_SMOS_2015-2020/", smoslist[[1]]))
 
 # Save metadata to a text file
 {
-  sink(paste0("../Data_Raw/SeaIceThickness_SMOS_2015-2020/", substr(thicklist[[2]], 1, 65),".txt"))
+  sink(paste0("../Data_Raw/SeaIceThickness_SMOS_2015-2020/", substr(smoslist[[1]], 1, 65),".txt"))
   print(ice)
   sink()
 }
 
-# Read lat lon and time for each observation
-lon <- ncvar_get(ice, "lon")
-lat <- ncvar_get(ice, "lat", verbose = F)
-t <- ncvar_get(ice, "time")
-
-head(lon)
-
-# Read in data from the wind variable and verify the dimensions of the array
-thick.array <- ncvar_get(ice, "analysis_sea_ice_thickness") # 3dim array
-dim(thick.array)
-
-conc.array <- ncvar_get(ice, "sea_ice_concentration") # 3dim array
-dim(conc.array)
-
-# Identify fill value and replace with NA
-fillvalue <- ncatt_get(ice, "sea_ice_concentration","_FillValue")
-fillvalue
-
-thick.array[thick.array == fillvalue$value] <- NA
-conc.array[conc.array == fillvalue$value] <- NA
-
 # Get projection information 
 prj <- ncatt_get(ice, "Lambert_Azimuthal_Grid","proj4_string")$value
 
-# Close netcdf file
-nc_close(ice)
+# Read lat lon and time for each observation
+lon <- ncvar_get(ice, "lon")
+lat <- ncvar_get(ice, "lat", verbose = F)
 
-# Flip to realign in correct direction
-conc.array2 <- aperm(conc.array, c(2,1))
+# Flip to correct orientation 
+lon2 <- aperm(lon, c(2,1))
+lat2 <- aperm(lat, c(2,1))
 
+# https://gis.stackexchange.com/questions/385881/remote-sensing-products-raster-extent-not-agreeing-with-specified-projection-in
+llv <- data.frame(long=c(lon2), lat=c(lat2))
+llv = st_as_sf(llv, coords=1:2)
+st_crs(llv)=4326
+llpolar = st_transform(llv,prj)
 
-# Make a raster of all values 
-icecon <- raster(conc.array2, xmn=min(lon), xmx=max(lon), ymn=min(lat), ymx=max(lat), crs=CRS("+proj=longlat +datum=WGS84 +no_defs")) %>% 
-  # flip(direction="x") %>%
-  flip(direction="y") %>%
-  raster::projectRaster(crs=prj) 
+nc_close()
 
-test <- raster::projectRaster(icecon, crs=crs(prj))
+# Function to process individual netcdfs into raster layers
+smosprocess <- function(netcdf, newprj = AA, croparea = aisbounds, llpolar){
+# Open netcdf file 
 
-aisbounds_laea <- st_transform(aisbounds, prj) 
-
-test <- raster::crop(icecon, aisbounds)
-
-test <- raster::projectRaster(from=icecon, to=sumstack[[1]], res=raster::res(icecon))
-
+  ice <- nc_open(paste0(wd,"Data_Raw/SeaIceThickness_SMOS_2015-2020/", netcdf))
+  
+  # Read lat lon and time for each observation
+  t <- ncvar_get(ice, "time")
+  
+  # Read in data from the wind variable and verify the dimensions of the array
+  thick.array <- ncvar_get(ice, "analysis_sea_ice_thickness") # 3dim array
+  dim(thick.array)
+  
+  conc.array <- ncvar_get(ice, "sea_ice_concentration") # 3dim array
+  dim(conc.array)
+  
+  # Identify fill value and replace with NA
+  fillvalue <- ncatt_get(ice, "sea_ice_concentration","_FillValue")
+  fillvalue
+  
+  thick.array[thick.array == fillvalue$value] <- NA
+  conc.array[conc.array == fillvalue$value] <- NA
+  
+  # Get projection information 
+  prj <- ncatt_get(ice, "Lambert_Azimuthal_Grid","proj4_string")$value
+  
+  # Close netcdf file
+  nc_close(ice)
+  
+  # Flip to realign in correct direction
+  conc.array2 <- aperm(conc.array, c(2,1))
+  thick.array2 <- aperm(thick.array, c(2,1))
+  
+  # Make a raster of all ice concentration values 
+  icethick <- raster(thick.array2)
+  extent(icethick) <- extent(llpolar)
+  crs(icethick) <- prj
+  
+  # icethickbs <- raster::projectRaster(icethick, crs=newprj, res = raster::res(icethick)) %>% 
+  #   raster::crop(y=croparea)
 
   
-  raster::projectRaster(crs=prj) 
+  # Make a raster of all ice thickness values 
+  icecon <- raster(conc.array2)
+  extent(icecon) <- extent(llpolar)
+  crs(icecon) <- prj
+  
+  # iceconbs <- raster::projectRaster(icecon, crs=newprj, res = raster::res(icecon)) %>% 
+  #   raster::crop(y=croparea)
+  
+  
+  # Convert date from seconds since 01/01/1970 to yyyy-mm-dd format
+  t2 <- as.POSIXct("1978-01-01 00:00")+as.difftime(t,units="secs")
+  t2 <- format(t2, "%Y-%m-%d")
+  
+  names(icecon) <- t2
+  names(icethick) <- t2
+  
+  return(list(icecon, icethick))
 
-# Convert date from seconds since 01/01/1970 to yyyy-mm-dd format
-t2 <- as.POSIXct("1900-01-01 00:00")+as.difftime(t,units="hours")
-t2 <- format(t2, "%G-W%V")
+}
+
+start <- proc.time()
+
+output <- lapply(1:length(smoslist), function(x){smosprocess(smoslist[[x]], llpolar=llpolar)})
+
+icecons <- sapply(output, "[[", 1) %>% 
+            raster::stack() %>% 
+            raster::projectRaster(crs=AA, res = raster::res(.)) %>% 
+            raster::crop(y=aisbounds) %>% 
+            raster::stackApply(indices=smosmonths, fun=sum)
+icethicks <- sapply(output, "[[", 2) %>% 
+            raster::stack() %>% 
+            raster::projectRaster(crs=AA, res = raster::res(.)) %>% 
+            raster::crop(y=aisbounds) %>% 
+            raster::stackApply(indices=smosmonths, fun=sum)
+
+writeRaster(icecons, "../Data_Processed/IceConcentration_SMOS.tif")
+saveRDS(icecons, "../Data_Processed/IceConcentration_SMOS.rds")
+writeRaster(icethicks, "../Data_Processed/IceThickness_SMOS.tif")
+saveRDS(icethicks, "../Data_Processed/IceThickness_SMOS.rds")
+
+proc.time() - start
 
 ######################################################################################
 # Load in all sea ice thickness files 
